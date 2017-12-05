134feb0c85bb21b1ee230638abdaafcb1ce93f77
tabokie <james_199@126.com>
2017-12-05 17:58:50 +0800

    Fix bug in dual pivot sort.

--- Sorting/Dual Pivot.c
+++ Sorting/Dual Pivot.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// with bug in exchange
 void DualPivotSorting(int* raw, int num){
 	
 	if (num > 2){
@@ -34,6 +35,46 @@ void DualPivotSorting(int* raw, int num){
 				Exchange(raw, lowidx, highidx);
 				if (highidx < pivot2 - 1){
 					Exchange(raw, pivot2, pivot2 - 1);
+				}
+				Exchange(raw, highidx, pivot2);
+				pivot2--;
+				highidx--;
+			}
+		}
+		//bug
+		DualPivotSorting(raw, pivot1);
+		DualPivotSorting(raw + pivot1 + 1, pivot2 - pivot1 - 1);
+		DualPivotSorting(raw + pivot2 + 1, num - pivot2 - 1);
+	}
+	else if (num == 2){
+		if (raw[0] > raw[1]){
+			Exchange(raw, 0, 1);
+		}
+	}
+}
+
+void DualPivotSorting_without_random_pivot(int* raw, int num){
+	
+	if (num > 2){
+		int pivot1=0,pivot2=num-1;
+		if(raw[pivot1]>raw[pivot2])Exchange(raw,pivot1,pivot2);
+						
+		int value1 = raw[pivot1], value2 = raw[pivot2];
+		int lowidx = pivot1 + 1, highidx = pivot2 - 1;
+		while (lowidx <= highidx){
+			if (raw[lowidx] < value1){
+				Exchange(raw, lowidx, pivot1+1);
+				Exchange(raw,pivot1,pivot1+1);
+				lowidx++;
+				pivot1++;
+			}
+			else if (raw[lowidx] >= value1&&raw[lowidx] <= value2){
+				lowidx++;
+			}
+			else{
+				Exchange(raw, lowidx, highidx);
+				if (highidx < pivot2 - 1){
+					Exchange(raw, highidx, pivot2 - 1);
 				}
 				Exchange(raw, highidx, pivot2);
 				pivot2--;
